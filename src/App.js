9d739c5704c133bc52ac11112cb4e731b77e77c2
marco-amorim <marcoamorim95@hotmail.com>
2020-07-16 06:13:41 -0300

    :rocket: Todo App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from 'react'; // hooks!
 
-function App() {
+// function App() {
+// 	return (
+// 		<div>
+// 			<h1>Hello world!</h1>
+// 		</div>
+// 	);
+// }
+
+// const App = () => {
+// 	// destructuring because useState returns an array
+// 	const [name, setName] = useState('Marco');
+// 	const onNameChange = useCallback((event) => {
+// 		console.log(event.target.value);
+// 		setName(event.target.value);
+// 	}, []); // passing [] because it has no dependencies!
+// 	return (
+// 		<div>
+// 			<form>
+// 				<label>Enter your name:</label>
+// 				<input value={name} onChange={onNameChange} />
+// 			</form>
+// 			<h1>Hello {name}!</h1>
+// 		</div>
+// 	);
+// };
+
+const App = () => {
+	const [newTodo, setNewTodo] = useState('');
+	const [todos, setTodos] = useState([]);
+	const onNewTodoChange = useCallback((event) => {
+		// useCallback creates a cached version of a function!
+		setNewTodo(event.target.value);
+	}, []);
+
+	const formSubmitted = useCallback(
+		(event) => {
+			event.preventDefault();
+			if (!newTodo.trim()) return;
+			setTodos([
+				...todos,
+				{
+					id: todos.length + 1,
+					content: newTodo,
+					done: false,
+				},
+			]);
+			setNewTodo('');
+		},
+		[newTodo, todos]
+	); // if newTodo or todos ever change the callback is called!
+
+	useEffect(() => {
+		console.log('todos', todos);
+	}, [todos]);
+
+	const addTodo = useCallback(
+		(todo, index) => (event) => {
+			// this is a clojure!
+			const newTodos = [...todos];
+			newTodos.splice(index, 1, {
+				...todo,
+				done: !todo.done,
+			});
+			setTodos(newTodos);
+		},
+		[todos]
+	);
+
+	const removeTodo = useCallback(
+		(todo) => (event) => {
+			setTodos(todos.filter((otherTodo) => otherTodo !== todo));
+		},
+		[todos]
+	);
+
+	const markAllDone = useCallback(() => {
+		// create a copy of the array
+		// create a copy of each of the items
+		// update the done property to be true on each of the new items
+		const updatedTodos = todos.map((todo) => {
+			return {
+				...todo,
+				done: true,
+			};
+		});
+		setTodos(updatedTodos);
+	}, [todos]);
+
+	// create Unmark All button!
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<form onSubmit={formSubmitted}>
+				<label htmlFor="newTodo">Enter a Todo:</label>
+				<input
+					id="newTodo"
+					name="newTodo"
+					value={newTodo}
+					onChange={onNewTodoChange}
+				/>
+				<button>Add Todo</button>
+				<button onClick={markAllDone}>Mark All Done</button>
+			</form>
+			<ul>
+				{todos.map((todo, index) => (
+					<li key={todo.id}>
+						<div className="todoItem">
+							<span className={todo.done ? 'done' : ''}>{todo.content}</span>
+							<input
+								checked={todo.done}
+								type="checkbox"
+								onChange={addTodo(todo, index)}
+							/>
+						</div>
+						<button onClick={removeTodo(todo)}>Remove Todo</button>
+					</li> // uses key (has to be unique) attribute for Virtual DOM to know what should be updated! (doesn't show in browser html)
+				))}
+			</ul>
 		</div>
 	);
-}
+};
 
 export default App;
